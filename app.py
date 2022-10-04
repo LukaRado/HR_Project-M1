9e9b836438b5bcc749dac4a3f6b0670727ea1d24
LukaRado <112628465+LukaRado@users.noreply.github.com>
2022-10-04 23:45:03 +0200

    asd

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ with tab2:
    color_scale = alt.Scale(domain=['Male', 'Female'],
                         range=['#1f77b4', '#e377c2'])
 
-   left = base.transform_filter(
+   left = source.transform_filter(
     alt.datum.gender == 'Female'
    ).encode(
     y=alt.Y('ret_rate:O', axis=None),
