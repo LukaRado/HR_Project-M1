188c57e8e849839afadcdf7b55f32d8461caf03c
LukaRado <112628465+LukaRado@users.noreply.github.com>
2022-10-04 19:07:36 +0200

    cleaning

--- app.py
+++ app.py
@@ -3,13 +3,7 @@
 import pandas as pd
 import numpy as np
 import altair as alt
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import OneHotEncoder
-import itertools
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-import shap
-import pickle
+
 
 #Importing data
 data1 = pd.read_csv('/content/employee_survey_data.csv')
