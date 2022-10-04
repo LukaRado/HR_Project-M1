0ea9c927bc26c32cd1ef9d7ac904535b00bc684e
LukaRado <112628465+LukaRado@users.noreply.github.com>
2022-10-04 19:08:48 +0200

    cleaning

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ import altair as alt
 
 
 #Importing data
-data1 = pd.read_csv('/content/employee_survey_data.csv')
-data2 = pd.read_csv('/content/general_data.csv')
-data3 = pd.read_csv('/content/manager_survey_data.csv')
+data1 = pd.read_csv('employee_survey_data.csv')
+data2 = pd.read_csv('general_data.csv')
+data3 = pd.read_csv('manager_survey_data.csv')
 
 #Merge data
 data = data3.merge(data2, on="EmployeeID", how="right").merge(data1, on="EmployeeID", how="right")
