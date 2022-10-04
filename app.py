9dadb67c543dfdc76f8abe3290e3e1b08f799e78
LukaRado <112628465+LukaRado@users.noreply.github.com>
2022-10-04 15:26:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,9 @@
 
-# Installing & Importing Data
-#u stands for upgrade #q stands for quiet/not so much output 
-!pip install xgboost -U -q 
-!pip install sklearn -U -q
-!pip install shap -U -q
 
 #importing libraries
 import pandas as pd
 import numpy as np
 import altair as alt
-from xgboost import XGBRegressor
-from sklearn.metrics import mean_squared_error
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import OneHotEncoder
-import itertools
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-import shap
-import pickle
 
 
 data1 = pd.read_csv('employee_survey_data.csv')
@@ -116,9 +102,3 @@ o_per = data['PerformanceRating'].mean()
 
 m_age = data['Age'].mean()
 m_age
-# Splitting and Training 
-# Supervised ML
-# Pick Model and train it 
-# Unsupervised ML 
-# Pick Model and train it 
-# Evaluation
