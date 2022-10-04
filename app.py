b59a82756fc655fddde39b1e70cf0ffd691e4184
nikolajvester <Nikolaj.vester@gmail.com>
2022-10-04 23:13:47 +0200

    Text

--- app.py
+++ app.py
@@ -64,8 +64,10 @@ tab1, tab2, tab3 = st.tabs(["Overview", "HR Managererial Tool", "SML"])
 
 with tab1:
    st.header("Overview")
-   st.write("We have created a tool, which main purpose is for the HR department to gain an overview of the company." +
+   st.write("We have created a tool, which main purpose is for the HR department to gain an overview of the company.")
+   st.text("")
    st.write("We have done this through the use of important KPI's which we have supplimented with useful graphs and information required to gain a proper insight into the state of the company.")
+   st.text("")
    st.write("We have also used supervised machine learning to predict probability of attrition while showing specific indicators for a picked employee and unsupervised learning to help us understand clusters which might be formed within the company, as these clusters could signal action is required")
 
 
